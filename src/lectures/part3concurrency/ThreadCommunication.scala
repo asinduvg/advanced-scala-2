7892bd60fd9aa4835493914f2ae536b3abb5ca69
Asindu <ashvg.dev@gmail.com>
2021-05-28 07:50:41 +0530

    producer consumer level 3

--- src/lectures/part3concurrency/ThreadCommunication.scala
+++ src/lectures/part3concurrency/ThreadCommunication.scala
@@ -131,8 +131,6 @@ object ThreadCommunication extends App {
         Thread.sleep(random.nextInt(500))
 
       }
-
-
     })
 
     producer.start()
@@ -140,7 +138,74 @@ object ThreadCommunication extends App {
 
   }
 
-  prodConsLargeBuffer()
+  //  prodConsLargeBuffer()
+
+  /*
+      Prod-Cons level 3
+      producer1 -> [? ? ? ?] <- consumer1
+      producer2 ---^      ^---- consumer2
+   */
+
+  class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+    override def run(): Unit = {
+      val random = new Random
+
+      while (true) {
+        buffer.synchronized {
+
+          while (buffer.isEmpty) {
+            println(s"[consumer $id] buffer is empty, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least one value in the buffer
+          val x = buffer.dequeue()
+          println(s"[consumer $id] consumed " + x)
+          buffer.notify() // wake up somebody (producer or consumer)
+        }
+
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
+      val random = new Random
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+
+          while (buffer.size == capacity) {
+            println(s"[producer $id] buffer is full, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least one empty space in the buffer
+          println(s"[producer $id] producing " + i)
+          buffer.enqueue(i)
+
+          buffer.notify() // wake up somebody (producer or consumer)
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(500))
+
+      }
+    }
+  }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+    (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+
+  }
+
+  multiProdCons(3, 3)
 
 }
 
