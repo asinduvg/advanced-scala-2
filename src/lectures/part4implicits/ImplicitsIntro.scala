c51f8d379d77edc2f6c883547eec393312a6dd55
Asindu <ashvg.dev@gmail.com>
2021-06-05 10:03:20 +0530

    enter implicits

---
+++ src/lectures/part4implicits/ImplicitsIntro.scala
@@ -0,0 +1,22 @@
+package lectures.part4implicits
+
+import scala.language.implicitConversions
+
+object ImplicitsIntro extends App {
+
+  case class Person(name: String) {
+    def greet = s"Hi my name is $name"
+  }
+
+  implicit def fromStringToPerson(str: String): Person = Person(str)
+
+  println("Peter".greet) // println(fromStringToPerson("Person").greet)
+
+  // implicit parameters
+  def increment(x: Int)(implicit amount: Int) = x + amount
+
+  implicit val defaultAmount: Int = 10
+
+  println(increment(15)) // NOT default args
+
+}
